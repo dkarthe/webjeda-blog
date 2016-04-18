1018ab718b6519e480e728130a8201a5069f8b59
RedGadgets <info@redgadgets.com>
2016-04-18 15:40:37 +0530

    no message

--- js/css-def.js
+++ js/css-def.js
@@ -2,7 +2,7 @@ var cb = function() {
 var l = document.createElement('link'); l.rel = 'stylesheet';
 var m = document.createElement('link'); m.rel = 'stylesheet';
 var n = document.createElement('link'); n.rel = 'stylesheet';
-l.href = '/css/main.css?v2';
+l.href = '/css/main.css';
 m.href = 'https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,600,900';
 n.href = 'https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css';
 var h = document.getElementsByTagName('head')[0]; h.parentNode.insertBefore(l, h);
