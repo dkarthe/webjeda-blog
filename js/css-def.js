3c9978948e79b17b16de76103f3abb74ddbd891a
RedGadgets <info@redgadgets.com>
2016-04-25 19:54:46 +0530

    no message

--- js/css-def.js
+++ js/css-def.js
@@ -1,11 +1,8 @@
 var cb = function() {
-var l = document.createElement('link'); l.rel = 'stylesheet';
 var m = document.createElement('link'); m.rel = 'stylesheet';
 var n = document.createElement('link'); n.rel = 'stylesheet';
-l.href = '/css/main.css';
 m.href = 'https://fonts.googleapis.com/css?family=Droid+Serif:400';
 n.href = 'https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css';
-var h = document.getElementsByTagName('head')[0]; h.parentNode.insertBefore(l, h);
 var i = document.getElementsByTagName('head')[0]; i.parentNode.insertBefore(m, i);
 var j = document.getElementsByTagName('head')[0]; j.parentNode.insertBefore(n, j);
 };
