7674aa63a37f83d744720c0a515935c00bb44708
RedGadgets <info@redgadgets.com>
2016-05-12 05:32:36 +0530

    no message

--- js/css-def.js
+++ js/css-def.js
@@ -1,12 +1,2 @@
-var cb = function() {
-var l = document.createElement('link'); l.rel = 'stylesheet';
-var m = document.createElement('link'); m.rel = 'stylesheet';
-l.href = '/css/main.css';
-m.href = 'https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css';
-var h = document.getElementsByTagName('head')[0]; h.parentNode.insertBefore(l, h);
-var i = document.getElementsByTagName('head')[0]; i.parentNode.insertBefore(m, i);
-};
-var raf = requestAnimationFrame || mozRequestAnimationFrame ||
-webkitRequestAnimationFrame || msRequestAnimationFrame;
-if (raf) raf(cb);
-else window.addEventListener('load', cb);
+var cb=function(){var a=document.createElement("link");a.rel="stylesheet";var b=document.createElement("link");b.rel="stylesheet";a.href="/css/main.css";b.href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css";var c=document.getElementsByTagName("head")[0];c.parentNode.insertBefore(a,c);a=document.getElementsByTagName("head")[0];a.parentNode.insertBefore(b,a)},raf=requestAnimationFrame||mozRequestAnimationFrame||webkitRequestAnimationFrame||msRequestAnimationFrame;
+raf?raf(cb):window.addEventListener("load",cb);
